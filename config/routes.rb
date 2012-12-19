be51c09a9df7ef6df135f710512eaf78428893aa
Sterling Zerr <sterlingzerr@gmail.com>
2012-12-18 19:03:51 -0700

    Add about link and about page

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,11 @@ Bonediva::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'main#index'
+
+ #aasdf match "/about", :controller => 'main', :action => 'about'
+
+  get '/about', :to => 'main#about'
 
   # See how all your routes lay out with "rake routes"
 
