bd3b5b1692404abd1906b4433eb261acd7172e9e
Alex Zerr <thwarts@gmail.com>
2012-12-21 12:22:06 -0700

    Start work on main controller  started working on adding some things to the main controller. also I started adding a few more things to the user model.

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,42 @@
+class MainController < ApplicationController
+  respond_to :html, :js
+
+  def index
+    @pictures = Picture.find.(:all, :order => date) 
+    @user     = User.find(:all, :order => 'id DESC') 
+
+  end
+
+  def edit
+    @pictures = Picture.find(params[:id])
+    @user     = User.find(params[:id])
+  end
+
+  def create
+
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if User.update(@user.id, user_params)
+      redirect_to @user, notice: { "#{@user.name} was succesfully updated" } 
+    end
+
+  end
+
+  def delete
+
+  end
+
+  def destroy
+
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @comments = @user.comments.order('id DESC').limit(20)
+    @pictures = picture.find(params[:id])
+
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   validate :name, presence: true, uniqueness: true
   validate :email, presence: true, uniqueness: true
-  validate :password, presence: true, 
+  validate :password, presence: true,
+
+
+    has_many :comments
   
 end
