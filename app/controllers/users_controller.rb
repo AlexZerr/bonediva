724b4a7916ffd44b2d53c08624dfd63297a80dd8
Alex Zerr <thwarts@gmail.com>
2012-12-23 00:32:06 -0700

    Work on UserController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,19 @@ class UsersController < ApplicationController
   before_filter :ensure_authorized, only: [ :edit, :update, :delete ] 
 
 
-
+#Should I just have this enherrit from the MainController?
 
 
   def show
+    @user     = User.find(params[:id])
+    @pictures = Picture.order('id desc').limit(20) 
 
   end
 
 
   def edit
+    @user = User.find(params[:id])
+    @picture = Picture.find(params[:id])
 
   end
 
@@ -23,7 +27,9 @@ class UsersController < ApplicationController
 
 
   def create
-
+    @error = []
+    @user = User.new(params[:user])
+    @error << "Field cannot be blank" if user_params.blank?
   end
 
 
