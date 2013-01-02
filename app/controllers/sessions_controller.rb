8db89608de5380d81769af506de9057fe2856faf
Alex Zerr <thwarts@gmail.com>
2013-01-02 15:41:36 -0700

    Finish working on sessions login should work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,14 @@
 class SessionsController < ApplicationController
 
  
-  def index
-    
-  end
-
 
   def new
+
   end 
 
 
  def create
-   @user = User.find_by_email(:email)
+   @user = User.find_by_email(params[:email])
    if @user.authenticate(params[:password])
      session[:user_id] = @user.id
      redirect_to root_url, :notice => "Logged In"
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   accepts_nested_attributes_for :pictures
 
   def self.authenticate(email, password)
-    find_by_email(email).try(:authenticate, password)
+    find_by_email(email).try(authenticate: password)
   end
 
 end
