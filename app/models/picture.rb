1e5bd20eb44645131574983ad625b956462dd414
Alex Zerr <thwarts@gmail.com>
2013-01-03 22:44:50 -0700

    Try to get pictures to upload still a mass assignment error

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,8 +1,9 @@
 class Picture < ActiveRecord::Base
-  attr_accessible :description, :title, :image, :remote_image_url
+  #attr_accessible :image, :remote_image_url
   mount_uploader :image, ImagesUploader
   accepts_nested_attributes_for :image
 
+  has_many :images
   belongs_to :user
 
 end
