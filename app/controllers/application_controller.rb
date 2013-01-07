1993c3617dd3b47438262c024c6b6f7a5c785882
Sterling Zerr <sterlingzerr@gmail.com>
2013-01-07 15:33:43 -0700

    Styling the mid_component

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  def index
-    @user = current_user
-  end
 
  # def set_current_user 
   #  Authorization.current_user = current_user
@@ -13,7 +10,7 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-  @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
 
