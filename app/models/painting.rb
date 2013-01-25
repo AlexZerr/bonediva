489ce63ad4e442a43a5eb9e08af26bd37b19426e
Alex Zerr <thwarts@gmail.com>
2013-01-25 16:49:42 -0700

    Add polymorphic relationship.

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -2,6 +2,7 @@ class Painting < ActiveRecord::Base
   mount_uploader :image, ImagesUploader
   attr_accessible :image, :description, :title, :user_id, :remote_image_url, :id
 
+  belongs_to :paintable, polymorphic: true
   validates :title, presence: true
   
   belongs_to :user
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@
 class Product < ActiveRecord::Base
   attr_accessible :name, :description, :price, :size
 
+  has_many :paintings, as: :paintable
+
   validates :name, presence: true
   validates :price, presence: true
   validates :size, presence: true
