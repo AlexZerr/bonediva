0b1a07eef58ad0a0b2f4c629f4795cd2902db16b
Alex Zerr <thwarts@gmail.com>
2013-01-25 15:20:22 -0700

    Products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130122212717) do
+ActiveRecord::Schema.define(:version => 20130125220543) do
 
   create_table "paintings", :force => true do |t|
     t.string   "title"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(:version => 20130122212717) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.integer  "user_id"
+  end
+
+  create_table "products", :force => true do |t|
+    t.string  "name",        :null => false
+    t.text    "description"
+    t.decimal "price",       :null => false
+    t.string  "size",        :null => false
+    t.integer "user_id"
+    t.integer "category_id"
   end
 
   create_table "users", :force => true do |t|
