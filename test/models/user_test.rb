f407da625eaacef50afb673aa78cc036d5ec1e88
Alex Zerr <thwarts@gmail.com>
2013-02-11 20:23:26 -0700

    Add destroy to users_test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,6 +8,11 @@ describe User do
    @product = @user.products.new( name: "Productname", description: "Product description", price: "32", size: "8x10" )
  end
 
+  after do
+    @user.destroy!
+    @product.destroy!
+  end
+
  it " is a valid user" do
    assert @user.valid?, "this is a valid user"
  end
