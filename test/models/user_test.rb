e0a851da27f8afd9f3eba72c363918f95f4cf677
Alex Zerr <thwarts@gmail.com>
2013-02-11 20:21:32 -0700

    Users test user.id present

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,17 +1,18 @@
 require "test_helper"
 require "minitest/spec"
+require "capybara/rails"
 
 describe User do
   before do
    @user = User.new(name: "testie", email: "Test@example.com", password: "password", age: "22", username: "tessties" )
+   @product = @user.products.new( name: "Productname", description: "Product description", price: "32", size: "8x10" )
  end
 
  it " is a valid user" do
    assert @user.valid?, "this is a valid user"
  end
  it "creates a product as user" do
-   @product = @user.products.new( name: "Productname", description: "Product description", price: "32", size: "8x10" )
    assert @product.valid?, "this product is valid"
+   @product.user_id.must_be_same_as @user.id
  end
-
 end
