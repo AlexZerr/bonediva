5753b9227182cef64f65f6d64941fa74a1ccf7c3
Alex Zerr <thwarts@gmail.com>
2013-02-11 19:28:28 -0700

    working minitest for users 2 assertions 2 passes

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,9 +2,16 @@ require "test_helper"
 require "minitest/spec"
 
 describe User do
- it " is a valid user" do
+  before do
    @user = User.new(name: "testie", email: "Test@example.com", password: "password", age: "22", username: "tessties" )
+ end
+
+ it " is a valid user" do
    assert @user.valid?, "this is a valid user"
+ end
+ it "creates a product as user" do
+   @product = @user.products.new( name: "Productname", description: "Product description", price: "32", size: "8x10" )
+   assert @product.valid?, "this product is valid"
  end
 
 end
