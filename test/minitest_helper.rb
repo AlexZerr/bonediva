eb6e9dfa83e41c55827c2ff8f9796251ac155e74
Alex Zerr <thwarts@gmail.com>
2013-02-11 17:38:53 -0700

    Re-do minitests? dont know why that happend.

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -3,7 +3,10 @@ require File.expand_path('../../config/environment', __FILE__)
 
 require "minitest/autorun"
 require "minitest/rails"
+<<<<<<< HEAD
 require "minitest/spec"
+=======
+>>>>>>> Re-do minitests? dont know why that happend.
 
 # Uncomment if you want Capybara in accceptance/integration tests
 # require "minitest/rails/capybara"
