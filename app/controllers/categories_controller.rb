20e99a05cb84ae8b503da1e83da4423001fa5539
Alex Zerr <thwarts@gmail.com>
2013-03-02 01:44:41 -0700

    bump

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,7 +7,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
-    @cat_paintings = @category.paintings
+    @cat_paintings = Category.paintings
   end
 
   def new
