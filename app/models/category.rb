7e1dde8cb23ffce0f54adb91728430e5cdb43645
Alex Zerr <thwarts@gmail.com>
2013-03-03 17:17:33 -0700

    Work on category featured painting and polymorphism.

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ActiveRecord::Base
   
   has_many :products
   has_many :paintings, as: :paintable
-  has_one :painting
+  belongs_to :featured_painting, class_name: "Painting", foreign_key: :featured_painting_id
 
   validates :name, presence: true
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130217062556) do
+ActiveRecord::Schema.define(:version => 20130303084246) do
 
   create_table "categories", :force => true do |t|
     t.string  "name"
     t.string  "description"
-    t.integer "painting_id"
+    t.integer "featured_painting_id"
   end
 
   create_table "paintings", :force => true do |t|
