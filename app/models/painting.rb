97452c022e8ff3ca8c883dfbaf27f579c929826e
Alex Zerr <thwarts@gmail.com>
2013-03-14 01:50:23 -0600

    Work on getting add as featured to work with just main product image.

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,6 +1,7 @@
 class Painting < ActiveRecord::Base
   mount_uploader :image, ImagesUploader
-  attr_accessible :image, :description, :title, :user_id, :remote_image_url, :id, :paintable_type, :paintable_id, :home, :category_id
+  attr_accessible :image, :description, :title, :user_id, :remote_image_url, :id, 
+    :paintable_type, :paintable_id, :home, :category_id, :primary_painting
 
   belongs_to :paintable, polymorphic: true
   belongs_to :category
