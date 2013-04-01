c11d4bee3b54a5d1cf7db34098d7fdf03b3410d6
Alex Zerr <thwarts@gmail.com>
2013-04-01 14:49:03 -0600

    Create initialize cart method for before_filters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,4 +24,14 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
 
+    def initialize_cart
+    if session[:cart_id]
+      @cart = Cart.find(session[:cart_id])
+    else
+      @cart = Cart.create
+      session[:cart_id] = @cart.id
+    end
+  end
+
+
 end
