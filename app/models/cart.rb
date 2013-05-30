29f8277548b9d58336a53e9501f63ce6bb871a5a
Alex Zerr <thwarts@gmail.com>
2013-05-29 23:09:47 -0600

    Validate and style

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,7 @@
 class Cart < ActiveRecord::Base
 
+  validates :product_id, presence: true, uniqueness: { scope: [:cart_id] }
+
   has_many :cart_items
   has_many :products, through: :cart_items
   has_one :order
