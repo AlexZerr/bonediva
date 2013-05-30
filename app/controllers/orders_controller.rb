e8d24902351a534a0c065df3bd8519df585caaa5
Alex Zerr <thwarts@gmail.com>
2013-05-29 22:34:51 -0600

    A little styling on orders and carts

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,15 +7,16 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(params[:order])
     if @order.save
-      redirect_to user_cart_order_path(current_user,current_user.carts.last, @order), notice: "Confirmation"
+      redirect_to user_cart_order_path(current_user,current_user.carts.last, @order), notice: "CHECKOUT"
     else
       flash "error"
     end
   end
 
   def show
+    @order = Order.find(params[:id])
     @user = User.find(params[:user_id])
-    @cart = @user.carts.last
+    @cart = Cart.find(params[:cart_id])
     @cart_items = @cart.cart_items
     @price = CartItem.where(cart_id: @cart.id).sum(:price)
   end
