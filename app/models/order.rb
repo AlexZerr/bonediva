51b409ea9dbf27a7ba9d7d888d32e385a2aadcf5
Alex Zerr <thwarts@gmail.com>
2013-06-01 17:08:19 -0600

    total_price to order table, price_in_cents

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,9 @@ class Order < ActiveRecord::Base
   attr_accessor :card_number, :card_verification
 belongs_to :cart
 has_many :cart_items
+
+
+  def price_in_cents
+    (self.total_price*100).round
+  end
 end
