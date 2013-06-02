cb316d67c85ecf2b98cda215acacd6a028a9fc38
Alex Zerr <thwarts@gmail.com>
2013-06-02 00:16:00 -0600

    Setup Order model for ActiveMerchant.

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,11 +3,53 @@ class Order < ActiveRecord::Base
 belongs_to :cart
 has_many :cart_items
 
+  def purchase
+    response = GATEWAY.purchase(price_in_cents, credit_card, purchase_options)
+
+    #transactions.create!(:action => "purchase", :amount => price_in_cents, :response => response)
+    
+    cart.update_attribute(:purchased_at, Time.now) if response.success?
+    response.success?
+  end
 
   def price_in_cents
     (self.total_price*100).round
   end
 
   private
+
+  def purchase_options
+    {
+      ip: ip_address,
+      billing_address: {
+        name:     "#{first_name} #{last_name}",
+        address:  address,
+        city:     city,
+        state:    state,
+        country:  country,
+        zip:      zip_code
+      }
+    }
+  end
+
+    def validate_card
+    unless credit_card.valid?
+      credit_card.errors.full_messages.each do |message|
+        errors.add_to_base message
+      end
+    end
+  end
+
+    def credit_card
+    @credit_card ||= ActiveMerchant::Billing::CreditCard.new(
+      :type               => card_type,
+      :number             => card_number,
+      :verification_value => card_verification,
+      :month              => card_expires_on.month,
+      :year               => card_expires_on.year,
+      :first_name         => first_name,
+      :last_name          => last_name
+    )
+  end
 
 end
