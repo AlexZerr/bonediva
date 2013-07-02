26e7f1ae57126a6a983cafee3b6dbf0df77e3de0
Alex Zerr <thwarts@gmail.com>
2013-07-01 21:53:06 -0600

    Create BlogPost#new/create

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -3,10 +3,16 @@ class BlogPostsController < ApplicationController
   before_filter :ensure_admin, only: [:new, :create]
 
   def new
-
+    @user = User.where( email: "lisaluree@gmail.com" )
+    @blog_post = @user.blog_posts.new(params[:blog_post])
   end
 
   def create
+    @user = User.where( email: "lisaluree@gmail.com" )
+    @blog_post = @user.blog_posts.new(params[:blog_post])
+    if @blog_post.save
+      redirect_to :back, notice: "#{@blog_post.title} has been created"
+    end
 
   end
 
