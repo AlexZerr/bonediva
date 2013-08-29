d1372f7490e05387d8011a42c991ef6971e995c6
Alex Zerr <thwarts@gmail.com>
2013-08-29 14:01:01 -0600

    Working AWS upload

--- app/uploaders/images_uploader.rb
+++ app/uploaders/images_uploader.rb
@@ -29,11 +29,8 @@ class ImagesUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-   def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-    "/app/public/uploads/missing_img.jpg"
-   end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,14 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAIFYLQ35TN46SBRAQ',                        # required
-    :aws_secret_access_key  => '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
-',                        # required
-   # :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
+    :aws_access_key_id      => 'AKIAJMQYD6WALCBUD4GA',                        # required
+    :aws_secret_access_key  => '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',          # required
+    :region                 => 'us-west-2',                  # optional, defaults to 'us-east-1'
    # :hosts                  => 's3.example.com',             # optional, defaults to nil
    # :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
   }
-  config.fog_directory  = 'bonediva_development'                     # required
-  config.fog_public     = false                                   # optional, defaults to true
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+  config.fog_directory  = 'bonediva'                     # required
+  config.fog_public     = true                                   # optional, defaults to true
+  #config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
   config.storage = :fog
 end
