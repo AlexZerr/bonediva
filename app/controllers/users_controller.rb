54e583bc9e5836f85786c0c2370902c19242ef51
Alex Zerr <thwarts@gmail.com>
2013-08-29 20:03:37 -0600

    Heroku fix- remove mailer from user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,7 +54,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      UserMailer.registration_confirmation(@user).deliver
+      #UserMailer.registration_confirmation(@user).deliver
         @avatar = Avatar.new(params[:avatar])
           @avatar.name = @user.name
           @avatar.user_id = @user.id
