0fbbe0460b516b649aeb6931badae2e625ebfd07
Alex Zerr <thwarts@gmail.com>
2013-10-30 01:34:16 -0600

    Add pic uploading to blog_posts

--- app/uploaders/event_img_uploader.rb
+++ app/uploaders/event_img_uploader.rb
@@ -49,6 +49,9 @@ class EventImgUploader < CarrierWave::Uploader::Base
       process :resize_to_fill => [300,300]
     end
 
+    version :small do
+      process :resize_to_fill => [200,200]
+    end
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
