7c55267f180f624fd87418ee7c34cc04c084d8ea
Alex Zerr <thwarts@gmail.com>
2013-10-30 00:20:48 -0600

    Add images to blog_posts and restyle painting upload form a tad

--- app/uploaders/event_img_uploader.rb
+++ app/uploaders/event_img_uploader.rb
@@ -1,6 +1,11 @@
 # encoding: utf-8
+require 'carrierwave/processing/mini_magick'
 
 class EventImgUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  include CarrierWave::MimeTypes
+
+  require 'fog' 
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
@@ -11,13 +16,13 @@ class EventImgUploader < CarrierWave::Uploader::Base
   # include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :fog
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/new/blog_image/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -39,6 +44,10 @@ class EventImgUploader < CarrierWave::Uploader::Base
   # version :thumb do
   #   process :scale => [50, 50]
   # end
+   
+    version :standard do
+      process :resize_to_fill => [300,300]
+    end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
