bfa61af66ab4bd1dddcf3466a750d9ac5272a415
Alex Zerr <thwarts@gmail.com>
2013-11-15 05:22:25 -0700

    speed of drop

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,25 +1,25 @@
 $(function() {
   $('.category_container').mouseenter( function() {
-    $(this).find('.paintings').show(400);
+    $(this).find('.paintings').show(250);
   });
 });
 
  $(function() {
     $('.category_container').on('mouseleave',( function() {
-      $('.paintings').hide(400);
+      $('.paintings').hide(250);
     })); 
 });
 
 
 $(function() {
   $('#second_component').mouseenter( function() {
-    $(this).find('.paintings').show(400);
+    $(this).find('.paintings').show(250);
   });
 });
 
  $(function() {
     $('#second_component').on('mouseleave',( function() {
-      $('.paintings').hide(400);
+      $('.paintings').hide(250);
     })); 
 });
 
