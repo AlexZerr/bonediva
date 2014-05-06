1f571b7ae1fc7f8a5282843c0cf2256ae408a5d5
Alex Zerr <thwarts@gmail.com>
2014-05-05 18:41:27 -0600

    Reset bonediva to where I know where it is.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -13,10 +13,12 @@ class MainController < ApplicationController
     @new_picture = Picture.new(params[:picture])
     @blog_posts = BlogPost.where( "created_at >= ? AND created_at <= ?", 1.month.ago, Time.now).order( "created_at DESC" ) 
     @bone_user = User.find_by_email("lisaluree@gmail.com")
-    if @bone_user.paintings.last.paintable_type == "Product"
-      @new_painting = @bone_user.paintings.where(primary_painting: true).last
-    elsif @bone_user.paintings.last.paintable_type == "Category"
-      @new_painting = @bone_user.paintings.where(paintable_type: "Category").last
+    if @bone_user.paintings.present?
+      if @bone_user.paintings.last.paintable_type == "Product"
+        @new_painting = @bone_user.paintings.where(primary_painting: true).last
+      elsif @bone_user.paintings.last.paintable_type == "Category"
+        @new_painting = @bone_user.paintings.where(paintable_type: "Category").last
+      end
     end
     @categories = Category.all
   end
