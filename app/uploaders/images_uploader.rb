26e219218f3c2d68c9948c0137b799ce351562c1
Alex Zerr <thwarts@gmail.com>
2014-05-13 00:51:27 -0600

    get s3 working on ec2

--- app/uploaders/images_uploader.rb
+++ app/uploaders/images_uploader.rb
@@ -25,7 +25,7 @@ class ImagesUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/new/#{model.id}"
+    "uploads/new2/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
