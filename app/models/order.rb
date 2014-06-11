40236b6ff3880ff30996e1f4d367fc6a60ad14c7
Alex Zerr <thwarts@gmail.com>
2014-06-10 20:56:57 -0600

    err sux 2

--- app/models/order.rb
+++ app/models/order.rb
@@ -39,7 +39,7 @@ before_create :validate_card
 
     def credit_card
     @credit_card ||= ActiveMerchant::Billing::CreditCard.new(
-      :brand               => card_brand,
+      :card_type               => brand,
       :number             => card_number,
       :verification_value => card_verification,
       :month              => card_expires_on.month,
