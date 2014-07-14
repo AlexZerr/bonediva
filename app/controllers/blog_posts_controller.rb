23317112e64bae0b8e45dec70bf5e66efacc1162
Alex Zerr <thwarts@gmail.com>
2014-07-14 11:45:37 -0600

    blah

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -16,8 +16,8 @@ class BlogPostsController < ApplicationController
   def create
     if current_user.email == "bonediva@gmail.com"
     @user = User.find_by_email( "bonediva@gmail.com")
-    elsif current_user.email.in?( "thwarts@gmail.com", "akmclan@hotmail.com") 
-    @user = User.find_by_email( "akmclan@hotmail.com")
+    elsif current_user.email.in?( "Thwarts@gmail.com", "akmclan@hotmail.com") 
+    @user = User.find_by_email( "Thwarts@gmail.com")
     end
     @blog_post = @user.blog_posts.new(params[:blog_post])
     @blog_post.user_id = @user.id
