9291901d3a4473817e6045644f45e0811f8cffff
Alex Zerr <thwarts@gmail.com>
2014-07-14 10:41:41 -0600

    refactor send message to model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,11 +11,15 @@ class MessagesController < ApplicationController
     @message = @user.messages.new(params[:message])
     @message.user_email = @user.email
     if @message.save
-      UserMailer.contact_bonediva(@user, @message).deliver
+      #UserMailer.contact_bonediva(@user, @message).deliver
       redirect_to :root, notice: "Message has been sent!"
     else
       redirect_to :back, notice: "#{@message.errors.full_messages}"
     end
+  end
+  
+  def index
+    respond_with Message.all
   end
 
   def destroy
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,14 @@
 class Message < ActiveRecord::Base
+  after_save :send_bone_diva_message
 
   attr_accessible :title, :body, :user_email
   belongs_to :user
+
+
+  private
+  def send_bone_diva_message
+    user = self.user
+    UserMailer.contact_bonediva(user, self).deliver
+  end
 
 end
