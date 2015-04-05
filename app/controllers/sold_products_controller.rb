1a415ca94e921445016f79fe72a109fe5816505d
Alex Zerr <thwarts@gmail.com>
2015-04-05 13:42:34 -0600

    errors on sold product

--- app/controllers/sold_products_controller.rb
+++ app/controllers/sold_products_controller.rb
@@ -6,6 +6,11 @@ class SoldProductsController < ApplicationController
 
   def create
     @sold_product = SoldProduct.new(params[:sold_product])
+    if @sold_product.save
+      redirect_to sold_product_path(@sold_product)
+    else
+      redirect_to :back, notice: "#{@sold_product.errors.full_messages}"
+    end
   end
 
   def show
